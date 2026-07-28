287817642721ba3f17e07e05a9d1380ec31994e7
EUNSOLY <dmsthf9596@gmail.com>
2026-07-28 18:43:50 +0900

    추가 실습1: 화면은 필요없고 데이터만 필요할 때 - @ResponseBody 사용
    - @ResponseBody를 사용하면 반환값인 객체를 직렬화된 문자열(JSON) 형태로 반환
    - 클라이언트는 해당 데이터를 받아 역직렬화하여 User 객체로 다시 사용
    - Spring을 사용하면 이런 직렬화/역직렬화 변환 작업을 서블릿 단에서 자동으로 처리해준다

--- src/main/java/com/demo/controller/UserPageController.java
+++ src/main/java/com/demo/controller/UserPageController.java
@@ -1,30 +1,36 @@
 package com.demo.controller;
 
+import com.demo.entity.User;
+import com.demo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 
 @Controller
 @RequestMapping("/users") // 공통으로 들어갈 경로는 클래스 자체에 명시 가능
 public class UserPageController {
+    private final UserService userService = new UserService();
+
+//    @GetMapping(value = "")
+//    public ModelAndView userPage() {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("/users/list");
+//        return modelAndView;
+//    }
+
     @GetMapping(value = "")
-    public ModelAndView userPage() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("/users/list");
-        return modelAndView;
+    @ResponseBody
+    public List<User> userPage() {
+        return userService.findAll();
     }
 
-    @GetMapping(value = "/1/detail")
-    public ModelAndView detailPage() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("id", 1);
-        modelAndView.addObject("name", "Aaron");
-        modelAndView.addObject("age", 10);
-        modelAndView.addObject("job", "Developer");
-        modelAndView.addObject("specialty", "Backend");
-        modelAndView.setViewName("/users/detail");
-        return modelAndView;
+    @GetMapping(value = "/1")
+    @ResponseBody
+    public User detailPage() {
+        return userService.findById(1);
     }
 }
---
+++ src/main/java/com/demo/service/UserService.java
@@ -0,0 +1,30 @@
+package com.demo.service;
+
+import com.demo.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+    private static final List<User> USERS = new ArrayList<>(){
+        {
+            add(new User(1, "Aaron", 15, "Developer","Backend"));
+            add(new User(1, "Baron", 20, "Developer","Frontend"));
+            add(new User(1, "Caron", 30, "Developer","Infra"));
+            add(new User(1, "Daron", 25, "Developer","Designer"));
+            add(new User(1, "Earon", 21, "Developer","Backend"));
+        }
+    };
+
+
+    public List<User> findAll(){
+        return USERS;
+    }
+
+    public User findById(int id){
+        return USERS.stream()
+                .filter(user->user.getId() == id)
+                .findFirst()
+                .orElseThrow(()-> new RuntimeException("찾으시는 유저가 없습니다."));
+    }
+}
