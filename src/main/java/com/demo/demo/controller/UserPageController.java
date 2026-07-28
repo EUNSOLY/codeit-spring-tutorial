a0f984f4477a705c1ed198e028069bc3839acf51
EUNSOLY <dmsthf9596@gmail.com>
2026-07-28 18:26:42 +0900

    실습 5-12: UserController : HTTP Method 명시 @GetMapping 으로 RequestMappingHandlerMapping 등록
    - 5-11은 class에 RequestMapping 적용하는거라서 패스
    - RequestMapping + method를 합친 GetMapping을 통해 RequestMappingHandlerMapping 등록

--- src/main/java/com/demo/demo/controller/UserPageController.java
+++ src/main/java/com/demo/demo/controller/UserPageController.java
@@ -3,6 +3,7 @@ package com.demo.demo.controller;
 
 import com.demo.demo.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,14 +12,14 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/users") // 공통으로 들어갈 경로는 클래스 자체에 명시 가능
 public class UserPageController {
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @GetMapping(value = "")
     public ModelAndView userPage() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("/users/list");
         return modelAndView;
     }
 
-    @RequestMapping(value = "/1/detail", method = RequestMethod.GET)
+    @GetMapping(value = "/1/detail")
     public ModelAndView detailPage() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("id", 1);
