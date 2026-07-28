74fdfb6a48a19fc257e1a5476bed5ad5f1160a09
EUNSOLY <dmsthf9596@gmail.com>
2026-07-28 18:06:45 +0900

    실습 5-8: IndexController : WelcomePage 가 아닌 @RequestMapping 으로 HandlerMapping 등록

---
+++ src/main/java/com/demo/demo/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.demo.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Slf4j
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(){
+        log.info(" - IndexController 내부에 정의한 @RequestMapping 통해 페이지 반환");
+        log.info(" - HandlerMapping 우선순위에 따라 3순위인 WelcomePageHandlerMapping 까지 가지않고 1순위인 RequestMappingHandlerMapping 에서 처리");
+        return "index";
+    }
+}
