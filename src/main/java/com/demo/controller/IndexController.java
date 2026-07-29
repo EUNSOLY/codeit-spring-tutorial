2076c56f4df9cd98c95385605db2c48b53be1de9
EUNSOLY <dmsthf9596@gmail.com>
2026-07-29 15:42:18 +0900

    실습 5-20: @responsebody : 웹페이지(HTML) 반환(ViewResolver)이 아닌 웹데이터(JSON) 반환(MessageConverter)

--- src/main/java/com/demo/controller/IndexController.java
+++ src/main/java/com/demo/controller/IndexController.java
@@ -4,13 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
 public class IndexController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(){
+    @ResponseBody
+    public String index() {
         log.info(" - IndexController 내부에 정의한 @RequestMapping 통해 페이지 반환");
         log.info(" - HandlerMapping 우선순위에 따라 3순위인 WelcomePageHandlerMapping 까지 가지않고 1순위인 RequestMappingHandlerMapping 에서 처리");
         return "index";
