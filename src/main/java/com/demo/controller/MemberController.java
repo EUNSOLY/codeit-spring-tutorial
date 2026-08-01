64a11616e45cdb2aea69d00cfc8d1d8bf9a331c0
EUNSOLY <dmsthf9596@gmail.com>
2026-08-01 15:40:35 +0900

    refactor : 1차 응답 개선 : ResponseEntity 사용으로 HTTP 상태코드 적용

--- src/main/java/com/demo/controller/MemberController.java
+++ src/main/java/com/demo/controller/MemberController.java
@@ -9,6 +9,8 @@ import com.demo.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -22,6 +24,11 @@ import java.util.List;
 public class MemberController {
     private final MemberService memberService;
 
+    /**
+     * ======================================================
+     * ModelAndView
+     * ======================================================
+     */
     @GetMapping(value = "/users")
     public String getUsersPage(Model model) {
         List<MemberResponseDto> memberResponses = memberService.readAll();
@@ -43,101 +50,146 @@ public class MemberController {
         return "users/detail";
     }
 
+
+    /**
+     * ======================================================
+     * RestAPI
+     * ======================================================
+     */
     @PostMapping(value = "/api/users")
     @ResponseBody
-    public MemberResponseDto createUser(
+    public ResponseEntity<MemberResponseDto> createUser(
             @RequestBody MemberCreateRequestDto request
     ) {
-        MemberResponseDto memberResponse = memberService.create(request);
-        log.info("사용자 생성 :  User (id={}, name={}, age={}, job={}, email={})",
-                memberResponse.getId(), memberResponse.getName(),
-                memberResponse.getAge(), memberResponse.getJob(),
-                memberResponse.getEmail());
-        return memberResponse;
+        try {
+            MemberResponseDto memberResponse = memberService.create(request);
+            log.info("사용자 생성 :  User (id={}, name={}, age={}, job={}, email={})",
+                    memberResponse.getId(), memberResponse.getName(),
+                    memberResponse.getAge(), memberResponse.getJob(),
+                    memberResponse.getEmail());
+            return ResponseEntity
+                    .ok(memberResponse);
+
+        } catch (RuntimeException e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(null);
+        }
+
     }
 
     @PostMapping(value = "/api/users-all")
     @ResponseBody
-    public List<MemberResponseDto> createUsers(
+    public ResponseEntity<List<MemberResponseDto>> createUsers(
             @RequestBody @Valid List<MemberCreateRequestDto> requests
     ) {
-        List<MemberResponseDto> memberResponses = memberService.createAll(requests);
-        memberResponses.forEach(memberResponse -> {
-            log.info("사용자 다중 생성 :  User (id={}, name={}, age={}, job={}, email={})",
-                    memberResponse.getId(), memberResponse.getName(),
-                    memberResponse.getAge(), memberResponse.getJob(),
-                    memberResponse.getEmail());
-        });
-        return memberResponses;
+        try {
+            List<MemberResponseDto> memberResponses = memberService.createAll(requests);
+            memberResponses.forEach(memberResponse -> {
+                log.info("사용자 다중 생성 :  User (id={}, name={}, age={}, job={}, email={})",
+                        memberResponse.getId(), memberResponse.getName(),
+                        memberResponse.getAge(), memberResponse.getJob(),
+                        memberResponse.getEmail());
+            });
+            return ResponseEntity
+                    .ok(memberResponses);
+        } catch (RuntimeException e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(null);
+        }
+
     }
 
     @GetMapping(value = "/api/users")
     @ResponseBody
-    public List<MemberResponseDto> getUsers() {
-        List<MemberResponseDto> memberResponses = memberService.readAll();
-        memberResponses.forEach(memberResponse -> {
-            log.info("사용자 전체 조회 :  User (id={}, name={}, age={}, job={}, email={})",
-                    memberResponse.getId(), memberResponse.getName(),
-                    memberResponse.getAge(), memberResponse.getJob(),
-                    memberResponse.getEmail());
-        });
-
-        return memberResponses;
+    public ResponseEntity<List<MemberResponseDto>> getUsers() {
+        try {
+            List<MemberResponseDto> memberResponses = memberService.readAll();
+            memberResponses.forEach(memberResponse -> {
+                log.info("사용자 전체 조회 :  User (id={}, name={}, age={}, job={}, email={})",
+                        memberResponse.getId(), memberResponse.getName(),
+                        memberResponse.getAge(), memberResponse.getJob(),
+                        memberResponse.getEmail());
+            });
+            // 생성자로 하는 방법
+            return new ResponseEntity<>(memberResponses, HttpStatus.OK);
+        } catch (RuntimeException e) {
+//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @GetMapping(value = "/api/users/{id}")
     @ResponseBody
-    public MemberResponseDto getUser(
+    public ResponseEntity<MemberResponseDto> getUser(
             @PathVariable(required = true) Integer id
     ) {
-        MemberResponseDto memberResponse = memberService.read(id);
-        log.info("사용자 1건 조회 :  User (id={}, name={}, age={}, job={}, email={})",
-                memberResponse.getId(), memberResponse.getName(),
-                memberResponse.getAge(), memberResponse.getJob(),
-                memberResponse.getEmail());
-        return memberResponse;
+        try {
+            MemberResponseDto memberResponse = memberService.read(id);
+            log.info("사용자 1건 조회 :  User (id={}, name={}, age={}, job={}, email={})",
+                    memberResponse.getId(), memberResponse.getName(),
+                    memberResponse.getAge(), memberResponse.getJob(),
+                    memberResponse.getEmail());
+            return ResponseEntity.status(HttpStatus.OK).body(memberResponse);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @PatchMapping(value = "/api/users/{id}")
     @ResponseBody
-    public MemberResponseDto patchUser(
+    public ResponseEntity<MemberResponseDto> patchUser(
             @PathVariable(required = false) Integer id,
             @RequestParam(required = false) String name,
             @RequestParam(required = false) Integer age,
             @RequestParam(required = false) String job,
             @RequestParam(required = false) String email
     ) {
-        MemberUpdateRequestDto request = new MemberUpdateRequestDto(name, age, job, email);
-        MemberResponseDto memberResponse = memberService.update(id, request);
-        log.info("사용자 부분 수정 :  User (id={}, name={}, age={}, job={}, email={})",
-                memberResponse.getId(), memberResponse.getName(),
-                memberResponse.getAge(), memberResponse.getJob(),
-                memberResponse.getEmail());
-        return memberResponse;
+        try {
+            MemberUpdateRequestDto request = new MemberUpdateRequestDto(name, age, job, email);
+            MemberResponseDto memberResponse = memberService.update(id, request);
+            log.info("사용자 부분 수정 :  User (id={}, name={}, age={}, job={}, email={})",
+                    memberResponse.getId(), memberResponse.getName(),
+                    memberResponse.getAge(), memberResponse.getJob(),
+                    memberResponse.getEmail());
+            return ResponseEntity.status(HttpStatus.OK).body(memberResponse);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 
     @PutMapping(value = "/api/users/{id}")
     @ResponseBody
-    public MemberResponseDto putUser(
+    public ResponseEntity<MemberResponseDto> putUser(
             @PathVariable(required = false) Integer id,
             @ModelAttribute @Valid MemberPutRequestDto request
     ) {
-        MemberResponseDto memberResponse = memberService.update(id, request);
-        log.info("사용자 전체 수정 :  User (id={}, name={}, age={}, job={}, email={})",
-                memberResponse.getId(), memberResponse.getName(),
-                memberResponse.getAge(), memberResponse.getJob(),
-                memberResponse.getEmail());
-        return memberResponse;
+        try {
+            MemberResponseDto memberResponse = memberService.update(id, request);
+            log.info("사용자 전체 수정 :  User (id={}, name={}, age={}, job={}, email={})",
+                    memberResponse.getId(), memberResponse.getName(),
+                    memberResponse.getAge(), memberResponse.getJob(),
+                    memberResponse.getEmail());
+            return ResponseEntity.status(HttpStatus.OK).body(memberResponse);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @DeleteMapping(value = "/api/users/{id}")
     @ResponseBody
-    public void deleteUser(@PathVariable(required = true) Integer id) {
-        MemberResponseDto memberResponse = memberService.delete(id);
-        log.info("삭제 사용자 :  User (id={}, name={}, age={}, job={}, email={})",
-                memberResponse.getId(), memberResponse.getName(),
-                memberResponse.getAge(), memberResponse.getJob(),
-                memberResponse.getEmail());
+    public ResponseEntity<String> deleteUser(@PathVariable(required = true) Integer id) {
+        try {
+            MemberResponseDto memberResponse = memberService.delete(id);
+            log.info("삭제 사용자 :  User (id={}, name={}, age={}, job={}, email={})",
+                    memberResponse.getId(), memberResponse.getName(),
+                    memberResponse.getAge(), memberResponse.getJob(),
+                    memberResponse.getEmail());
+            return ResponseEntity.status(HttpStatus.OK).body("삭제 완료");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("삭제 실패.");
+        }
     }
 }
