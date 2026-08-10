3d0fb62ddf5d2c0e1bd3fb0e79138b027c3eade9
EUNSOLY <dmsthf9596@gmail.com>
2026-08-10 18:19:46 +0900

    feat : 각 도메인별 Repository 구현체에 대한 공통 IRepository 통해 OCP 원칙 적용

--- src/main/java/com/demo/repository/payment/PaymentRepository.java
+++ src/main/java/com/demo/repository/payment/PaymentRepository.java
@@ -1,6 +1,7 @@
 package com.demo.repository.payment;
 
 import com.demo.domain.payment.Payment;
+import com.demo.repository.IRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
@@ -15,20 +16,23 @@ import java.util.*;
  * - D : 단일 삭제
  */
 @Repository
-public class PaymentRepository {
+public class PaymentRepository implements IRepository<Integer, Payment> {
     private final static Map<Integer, Payment> PAYMENTS = new HashMap<>();
 
     // R : 전체 조회
+    @Override
     public List<Payment> findAll() {
         return PAYMENTS.values().stream().toList();
     }
 
     // R : 단일 조회
+    @Override
     public Optional<Payment> findById(Integer id) {
         return Optional.ofNullable(PAYMENTS.get(id));
     }
 
     // C : 단일 생성
+    @Override
     public Optional<Payment> create(Payment entity) {
         int id = entity.getId();
 
@@ -41,6 +45,7 @@ public class PaymentRepository {
     }
 
     // U : 댠일 갱신
+    @Override
     public Optional<Payment> update(Payment entity) {
         int id = entity.getId();
         if (Objects.isNull(PAYMENTS.get(id))) {
@@ -52,6 +57,7 @@ public class PaymentRepository {
     }
 
     // D : 전체 삭제
+    @Override
     public void delete(Integer id) {
         if (Objects.isNull(PAYMENTS.get(id))) {
             throw new RuntimeException("기존에 해당 아이디를 가진 결제가 존재하지 않습니다 - id : " + id);
