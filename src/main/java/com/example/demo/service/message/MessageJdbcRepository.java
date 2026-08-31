0351b2a7677a79ff99553cb34c551cd12b7d90ec
EUNSOLY <dmsthf9596@gmail.com>
2026-08-31 18:19:00 +0900

    실습 6-14: 회원가입 축하 Message 저장 시 Exception 발생 - 트랜잭션 동기화가 안되어 롤백 미적용
    - 메세지는 실패했지만 회원은 생성되어있음

--- src/main/java/com/example/demo/service/message/MessageJdbcRepository.java
+++ src/main/java/com/example/demo/service/message/MessageJdbcRepository.java
@@ -55,6 +55,11 @@ public class MessageJdbcRepository {
     }
 
     public List<Message> save(Integer userId, String message) throws SQLException {
+
+        if (true) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "트랜잭션 롤백 여부를 확인하기 위한 의도된 예외");
+        }
+        
         Connection connection = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
